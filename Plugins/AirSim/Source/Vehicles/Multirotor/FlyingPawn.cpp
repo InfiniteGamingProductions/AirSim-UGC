59ce3f9f7522573b614ea14c5a0260f32fe607db
Jacob Holyfield <31736942+Infin8Gamer1@users.noreply.github.com>
2021-04-19 19:36:27 -0700

    Initial Commit

---
+++ Plugins/AirSim/Source/Vehicles/Multirotor/FlyingPawn.cpp
@@ -0,0 +1,97 @@
+#include "FlyingPawn.h"
+#include "Components/StaticMeshComponent.h"
+#include "AirBlueprintLib.h"
+#include "common/Common.hpp"
+#include "Containers/UnrealString.h"
+
+AFlyingPawn::AFlyingPawn()
+{
+    bodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body Mesh"));
+	RootComponent = bodyMesh;
+    
+    pawn_events_.getActuatorSignal().connect_member(this, &AFlyingPawn::SetRotorSpeed);
+}
+
+void AFlyingPawn::BeginPlay()
+{
+    Super::BeginPlay();
+
+	initializeProps();
+}
+
+void AFlyingPawn::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+	
+    pawn_events_.getPawnTickSignal().emit(DeltaSeconds);
+}
+
+void AFlyingPawn::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    pawn_events_.getActuatorSignal().disconnect_all();
+
+    Super::EndPlay(EndPlayReason);
+}
+
+const common_utils::UniqueValueMap<std::string, APIPCamera*> AFlyingPawn::GetCameras() const
+{
+    common_utils::UniqueValueMap<std::string, APIPCamera*> uniqueValueMap;
+	
+	for (const TTuple<FString, APIPCamera*>& Element : cameras)
+	{
+		uniqueValueMap.insert_or_assign(TCHAR_TO_UTF8(*Element.Key), Element.Value);
+	}
+	
+	return uniqueValueMap;
+
+	// Old Implementation, Here for reference
+	/*cameras.insert_or_assign("front_center", camera_front_center_);
+    cameras.insert_or_assign("front_right", camera_front_right_);
+    cameras.insert_or_assign("front_left", camera_front_left_);
+    cameras.insert_or_assign("bottom_center", camera_bottom_center_);
+    cameras.insert_or_assign("back_center", camera_back_center_);
+
+    cameras.insert_or_assign("0", camera_front_center_);
+    cameras.insert_or_assign("1", camera_front_right_);
+    cameras.insert_or_assign("2", camera_front_left_);
+    cameras.insert_or_assign("3", camera_bottom_center_);
+    cameras.insert_or_assign("4", camera_back_center_);
+
+    cameras.insert_or_assign("", camera_front_center_);
+    cameras.insert_or_assign("fpv", camera_front_center_);*/
+}
+
+void AFlyingPawn::NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation, 
+    FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit)
+{
+    pawn_events_.getCollisionSignal().emit(MyComp, Other, OtherComp, bSelfMoved, HitLocation,
+        HitNormal, NormalImpulse, Hit);
+}
+
+void AFlyingPawn::SetRotorSpeed(const std::vector<MultirotorPawnEvents::RotorActuatorInfo>& rotor_infos)
+{
+    for (int32 rotor_index = 0; rotor_index < rotor_infos.size(); rotor_index++) {
+        URotatingMovementComponent* comp = rotatingMovements[rotor_index];
+        if (comp != nullptr) {
+            comp->RotationRate.Yaw = 
+                rotor_infos.at(rotor_index).rotor_speed * rotor_infos.at(rotor_index).rotor_direction *
+                180.0f / M_PIf * RotatorFactor;
+        }
+    }
+}
+
+void AFlyingPawn::initializeProps()
+{
+	if (rotatingMovements.Num() == 0 || propMeshes.Num() == 0)
+	{
+		return;
+	}
+	
+	for (int32 x = 0; x < rotatingMovements.Num() && x < propMeshes.Num(); x++)
+	{
+		if (rotatingMovements[x] != nullptr && propMeshes[x] != nullptr)
+		{
+			rotatingMovements[x]->SetUpdatedComponent(propMeshes[x]);
+		}
+	}
+}
---
+++ Plugins/AirSim/Source/Vehicles/Multirotor/FlyingPawn.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "GameFramework/RotatingMovementComponent.h"
+
+#include "PIPCamera.h"
+#include "common/common_utils/UniqueValueMap.hpp"
+#include "MultirotorPawnEvents.h"
+
+#include "FlyingPawn.generated.h"
+
+UCLASS()
+class AIRSIM_API AFlyingPawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    AFlyingPawn();
+	
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaSeconds) override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+    virtual void NotifyHit(class UPrimitiveComponent* MyComp, class AActor* Other, class UPrimitiveComponent* OtherComp, bool bSelfMoved, FVector HitLocation,
+        FVector HitNormal, FVector NormalImpulse, const FHitResult& Hit) override;
+
+    //Returns the camera objects from the pawn
+    const common_utils::UniqueValueMap<std::string, APIPCamera*> GetCameras() const;
+	
+	//Returns the pawn events object
+    FORCEINLINE MultirotorPawnEvents* GetPawnEvents() { return &pawn_events_; }
+	
+    //Used to set the prop speed
+    void SetRotorSpeed(const std::vector<MultirotorPawnEvents::RotorActuatorInfo>& rotor_infos);
+
+
+private:
+	//Functions
+
+	//Pairs rotating movement components to the prop meshes
+    UFUNCTION(BlueprintCallable, meta = (AllowPrivateAccess = "true"))
+	void initializeProps();
+    
+    //Unreal components
+	
+	//The root mesh of the drone
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* bodyMesh;
+
+	//Components used to rotate the props (Note: Must be added in blueprint and order matters start from back left prop and go clockwise)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TArray<URotatingMovementComponent*> rotatingMovements;
+
+	//Static Mesh Components for the props (Note: Must be added in blueprint and order must match rotating movements)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TArray<UStaticMeshComponent*> propMeshes;
+
+	//Holds Map of camera names to camera objects (Note: Must be added in blueprints)
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TMap<FString, APIPCamera*> cameras;
+
+	//Vars
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Debugging", meta = (AllowPrivateAccess = "true"))
+	float RotatorFactor = 1.0f;
+	
+    MultirotorPawnEvents pawn_events_;
+};
